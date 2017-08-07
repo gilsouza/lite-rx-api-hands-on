1da7750a2185cfa28398b1076fb15dcdbcd3f54f
gilmar.costa <gilmar.costa@BRRJOBITANL0989>
2017-08-07 17:36:27 -0300

    Inicio da parte 6.

--- src/main/java/io/pivotal/literx/Part06Request.java
+++ src/main/java/io/pivotal/literx/Part06Request.java
@@ -15,30 +15,36 @@ public class Part06Request {
 
 	ReactiveRepository<User> repository = new ReactiveUserRepository();
 
-//========================================================================================
+	// ========================================================================================
 
-	// TODO Create a StepVerifier that initially requests all values and expect 4 values to be received
+	// TODO Create a StepVerifier that initially requests all values and expect 4
+	// values to be received
 	StepVerifier requestAllExpectFour(Flux<User> flux) {
-		return null;
+		return StepVerifier.create(flux).expectNextCount(4).expectComplete();
 	}
 
-//========================================================================================
+	// ========================================================================================
 
-	// TODO Create a StepVerifier that initially requests 1 value and expects User.SKYLER then requests another value and expects User.JESSE.
+	// TODO Create a StepVerifier that initially requests 1 value and expects
+	// User.SKYLER then requests another value and expects User.JESSE.
 	StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
 		return null;
+
 	}
 
-//========================================================================================
+	// ========================================================================================
 
-	// TODO Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals
+	// TODO Return a Flux with all users stored in the repository that prints
+	// automatically logs for all Reactive Streams signals
 	Flux<User> fluxWithLog() {
 		return null;
 	}
 
-//========================================================================================
+	// ========================================================================================
 
-	// TODO Return a Flux with all users stored in the repository that prints "Starring:" on subscribe, "firstname lastname" for all values and "The end!" on complete
+	// TODO Return a Flux with all users stored in the repository that prints
+	// "Starring:" on subscribe, "firstname lastname" for all values and "The end!"
+	// on complete
 	Flux<User> fluxWithDoOnPrintln() {
 		return null;
 	}
